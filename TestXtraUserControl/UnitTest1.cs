78a0604140d07979de627b753559e62e6e5e1b30
Bjorn le Fevre <doggiehostmaster@gmail.com>
2013-10-17 16:25:35 +0200

    TestXtraUserControl dialog tester toegevoegd.

---
+++ TestXtraUserControl/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraLayout;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestXtraUserControl
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var userControl = new XtraUserControl { Width = 300, Height = 300, Dock = DockStyle.Fill };
+
+            var memoEdit = new MemoEdit { Text = "werkstroomTaakDefinitie.WerkInstructies" };
+            memoEdit.Properties.ReadOnly = true;
+            var annulerenButton = new SimpleButton { Text = "OK", DialogResult = DialogResult.OK };
+
+            var layoutControl = new LayoutControl { Dock = DockStyle.Fill };
+            var lci = layoutControl.AddItem(string.Empty, memoEdit);
+            lci.TextVisible = false;
+            layoutControl.AddItem(string.Empty, annulerenButton);
+            layoutControl.Controls.Add(memoEdit);
+            layoutControl.Controls.Add(annulerenButton);
+
+            userControl.Controls.Add(layoutControl);
+            var xtraForm = new XtraForm { Text = "_controller.HuidigObject.UserDisplay" };
+            xtraForm.Controls.Add(userControl);
+            var dialogResult = xtraForm.ShowDialog();
+            if (dialogResult == DialogResult.OK) xtraForm.Close();
+        }
+    }
+}
