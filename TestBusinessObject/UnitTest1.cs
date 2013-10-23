16c8d88f68044bde6f77ec35a4e0b5cb3acf186f
Bjorn le Fevre <doggiehostmaster@gmail.com>
2013-10-23 15:50:03 +0200

    BusinessObject test solution toegevoegd

---
+++ TestBusinessObject/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using Alure.WS.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Alure.Base.BL.Test;
+
+namespace TestBusinessObject
+{
+    [TestClass]
+    public class UnitTest1 : TestBase
+    {
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void IsPrimaireActieAndDirectStarten_WerkstroomActieDefinitieInit_IsFalse()
+        {
+            var werkstroomActieDefinitie = new WerkstroomActieDefinitie();
+            Assert.IsFalse(werkstroomActieDefinitie.IsPrimaireActie);
+            Assert.IsFalse(werkstroomActieDefinitie.DirectStarten);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void IsPrimaireActieEnabled_IsAutomatischUitvoeren_IsFalse()
+        {
+            var werkstroomActieDefinitie = new WerkstroomActieDefinitie
+                {
+                    AutomatischUitvoeren = true
+                };
+
+            var result = werkstroomActieDefinitie.IsPrimaireActieEnabled;
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void IsDirectStartenEnabled_IsAutomatischUitvoeren_IsFalse()
+        {
+            var werkstroomActieDefinitie = new WerkstroomActieDefinitie
+            {
+                AutomatischUitvoeren = true
+            };
+
+            var result = werkstroomActieDefinitie.IsDirectStartenEnabled;
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void IsAutomatischeActieEnabled_IsPrimaireActie_IsFalse()
+        {
+            var werkstroomActieDefinitie = new WerkstroomActieDefinitie
+            {
+                ActieSoort = WerkstroomActieDefinitie.ActieSoortWaardes.Stempel,
+                IsPrimaireActie = true
+            };
+
+            var result = werkstroomActieDefinitie.AutomatischUitvoeren;
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [TestCategory("UnitTest")]
+        public void IsDirectStartenEnabled_IsPrimaireActie_IsTrue()
+        {
+            var werkstroomActieDefinitie = new WerkstroomActieDefinitie {IsPrimaireActie = true};
+            var result = werkstroomActieDefinitie.IsDirectStartenEnabled;
+            Assert.IsTrue(result);
+        }
+    }
+}
